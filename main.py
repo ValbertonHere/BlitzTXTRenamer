0b1755702f9a456d15804012561e7fc73265c1f6
Roman "Valberton" Levchenko <romandogmc@gmail.com>
2024-12-08 14:26:38 +0300

    Строка подсасывается из имени webp файла.

--- main.py
+++ main.py
@@ -2,31 +2,35 @@ import os
 
 print('© Valberton 2018-2024\n')
 
-def main(folderPath, strToRename):
+def main(folderPath):
+
+    txtFiles = []
+    webpFiles = []
 
     for file in os.listdir(folderPath):
         if file.endswith('.txt'):
-            print(f'Обработка файла: {file}')
+            txtFiles.append(file)
+        
+        if file.endswith('.webp'):
+            webpFiles.append(file)
 
-            with open(f'{folderPath}/{file}', 'r') as file2r:
-                filelines = file2r.readlines()
-            
-            with open(f'{folderPath}/{file}', 'w') as file2w:
-                filelines[1] = strToRename + '\n'
-                file2w.writelines(filelines)
+    for idx, file in enumerate(txtFiles):
+        print(f'Обработка файла: {file}')
+
+        with open(f'{folderPath}/{file}', 'r') as file2r:
+            filelines = file2r.readlines()
+        
+        with open(f'{folderPath}/{file}', 'w') as file2w:
+            filelines[1] = webpFiles[idx] + '\n'
+            file2w.writelines(filelines)
 
 while True:
     folderPath = input('Введите имя рабочей папки (по умолчанию - папка выполнения скрипта): ')
-    strToRename = input('Введите строку для замены: ')
     
     if len(' '.join(folderPath.split())) == 0:
         folderPath = os.getcwd()
 
-    if len(' '.join(strToRename.split())) == 0:
-        print('\nНедопустимая строка для замены, повторение запроса...\n')
-        continue
-    
-    main(folderPath, strToRename)
+    main(folderPath)
 
     isExit = input('Выйти из программы? [Y/N] ')
     if isExit.lower() == 'y' or isExit.lower() == 'д':
