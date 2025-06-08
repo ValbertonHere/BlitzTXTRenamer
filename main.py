aafb5f314080ab7c99ae22b9fc8c085d5fd399b5
Roman "Valberton" Levchencko <romandogmc@gmail.com>
2025-06-08 13:20:26 +0300

    Хз что там, но что-то важное

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 print('© Valberton 2018-2024\n')
 
-def main(folderPath):
+def main(folderPath, strToRename=None):
 
     txtFiles = []
     webpFiles = []
@@ -21,16 +21,23 @@ def main(folderPath):
             filelines = file2r.readlines()
         
         with open(f'{folderPath}/{file}', 'w') as file2w:
-            filelines[1] = webpFiles[idx] + '\n'
+            if strToRename is not None:
+                filelines[1] = strToRename + '\n'
+            else:
+                filelines[1] = webpFiles[idx] + '\n'
             file2w.writelines(filelines)
 
 while True:
     folderPath = input('Введите имя рабочей папки (по умолчанию - папка выполнения скрипта): ')
+    strToRename = input('Введите строку для замены (для присваивания множеству txt одного webp-файла): ')
     
     if len(' '.join(folderPath.split())) == 0:
         folderPath = os.getcwd()
 
-    main(folderPath)
+    if len(' '.join(strToRename.split())) == 0:
+        strToRename = None
+
+    main(folderPath, strToRename)
 
     isExit = input('Выйти из программы? [Y/N] ')
     if isExit.lower() == 'y' or isExit.lower() == 'д':
